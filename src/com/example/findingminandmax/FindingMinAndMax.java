c2ba724ca9cfb03a4d41bfeeca9053330071d4dc
1Max58 <>
2023-11-08 18:09:50 +0200

    Creating an array with random elements implemented

--- src/com/example/findingminandmax/FindingMinAndMax.java
+++ src/com/example/findingminandmax/FindingMinAndMax.java
@@ -2,6 +2,12 @@ package com.example.findingminandmax;
 
 public class FindingMinAndMax {
     public static void main(String[] args) {
-        
+        System.out.println("This program creates a one-dimensional array with 10 elements and find the smallest and the largest element.");
+        int lowerLimit = 1;
+        int upperLimit = 100;
+        int[] nums = new int[10];
+        for (int i = 0; i < nums.length; i++) {
+            nums[i] = (int) (Math.random() * (upperLimit + 1 - lowerLimit) + lowerLimit);
+        }
     }
 }
