ff43a266df882dbe288e15e3d2da8a7274a795f9
1Max58 <>
2023-11-08 18:12:45 +0200

    Finding the smallest and largest element implemented

--- src/com/example/findingminandmax/FindingMinAndMax.java
+++ src/com/example/findingminandmax/FindingMinAndMax.java
@@ -9,9 +9,19 @@ public class FindingMinAndMax {
         for (int i = 0; i < nums.length; i++) {
             nums[i] = (int) (Math.random() * (upperLimit + 1 - lowerLimit) + lowerLimit);
         }
+        int maxValue = lowerLimit;
+        int minValue = upperLimit;
         for (int value : nums) {
             System.out.print(value + " ");
+            if (value > maxValue) {
+                maxValue = value;
+            }
+            if (value < minValue) {
+                minValue = value;
+            }
         }
         System.out.println();
+        System.out.println("The smallest number is " + minValue);
+        System.out.println("The largest number is " + maxValue);
     }
 }
